73dd2e5da231f624179a4483a4e508a366932e87
row-d <davidgomez2924@gmail.com>
2024-06-30 01:50:59 -0400

    feat: se agregaron datos para ambito_patrimonio y patrimonio_inmaterial en el codigo python

--- generar_datos.py
+++ generar_datos.py
@@ -2,23 +2,23 @@ from os import getenv
 from dotenv import load_dotenv
 import psycopg2
 from faker import Faker
-from dominios import *
+from valores import *
 
 def random_run(fake: Faker)-> int:
   return fake.unique.random_int(10000000,99999999)
 
 def patrimonio_inmaterial(cursor,nombre_elemento):
-  cursor.execute(f"INSERT INTO patrimonio_inmaterial (nombre_elemento) VALUES ('{nombre_elemento}')")
+  cursor.execute(f"INSERT INTO patrimonio_inmaterial (nombre_elemento) VALUES (%s)",(nombre_elemento,))
 
 def entidad(cursor,nombre,cod_comuna)-> int | None:
-  cursor.execute(f"INSERT INTO entidad (nombre,cod_comuna) VALUES ('{nombre}',{cod_comuna}) RETURNING id_entidad")
+  cursor.execute(f"INSERT INTO entidad (nombre,cod_comuna) VALUES (%s,%s) RETURNING id_entidad",(nombre,cod_comuna))
   result = cursor.fetchone()
   return result[0] if result else None
 
 def persona_natural(cursor,cod_comuna,rut,nombre_elemento,nombre,sexo,fecha_nacimiento,direccion,nombre_pueblo_ori,discapacidad):
   id_entidad = entidad(cursor,nombre,cod_comuna)
   if id_entidad is not None:
-    cursor.execute(f"INSERT INTO persona_natural (id_entidad,rut,nombre_elemento,nombre,sexo,fecha_nacimiento,direccion,nombre_pueblo_ori,discapacidad) VALUES ({id_entidad},'{rut}','{nombre_elemento}','{nombre}','{sexo}','{fecha_nacimiento}','{direccion}','{nombre_pueblo_ori}','{discapacidad}')")
+    cursor.execute(f"INSERT INTO persona_natural (id_entidad,rut,nombre_elemento,nombre,sexo,fecha_nacimiento,direccion,nombre_pueblo_ori,discapacidad) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)",(id_entidad,rut,nombre_elemento,nombre,sexo,fecha_nacimiento,direccion,nombre_pueblo_ori,discapacidad))
 
 
 if __name__ == "__main__":  
