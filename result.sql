4760a6795ef5b38357aa1634258ac7de6c2a79c3
row-d <davidgomez2924@gmail.com>
2024-07-02 19:44:24 -0400

    feat(vista): 3ra vista del proyecto

--- result.sql
+++ result.sql
@@ -1,26 +1,39 @@
--- TODO: generar datos para tablas: region, biblioteca_publica, biblioteca_escolar_cra, estudiante, establecimiento_educacional, artesania, artesano,atiende_año
-
 --VISTA
 /*
-V.3. 
+V.3.
 Cree una vista que contenga para cada región, la cantidad de bibliotecas escolares que tiene y la
 cantidad de bibliotecas públicas que tiene.
 */
---CREATE VIEW bibliotecas_regiones as 
-SELECT COUNT(id_biblioteca) FROM biblioteca_publica;
+CREATE OR REPLACE VIEW bibliotecas_por_region AS
+    (SELECT re.cod_region,
+
+         (SELECT count(*)
+          FROM biblioteca_publica pb
+          JOIN persona_juridica pj ON pb.id_persona_jur = pj.id_entidad
+          JOIN entidad ent USING(id_entidad)
+          JOIN comuna USING(cod_comuna)
+          JOIN provincia USING(cod_provincia)
+          WHERE provincia.cod_region = re.cod_region ) AS bibliotecas_publicas,
+
+         (SELECT count(*) AS bibliotecas_escolares_cra
+          FROM biblioteca_escolar_cra be
+          JOIN establecimiento_educacional est_ed USING(codigo_est)
+          JOIN comuna co USING(cod_comuna)
+          JOIN provincia USING(cod_provincia)
+          WHERE provincia.cod_region = re.cod_region) AS bibliotecas_escolares_cra
+     FROM region re);
 
 -- TRIGGER
 /*
-T.6. 
+T.6.
 Cree un trigger para que cada vez que un estudiante cambia de establecimiento en un año se modifique
 estadística de atendidos en ese año y nivel en las bibliotecas escolares que corresponden tanto al
 establecimiento antiguo como al nuevo.
-*/
--- FUNCIÓN
+*/ -- FUNCIÓN
 /*
-F.7. 
+F.7.
 Escriba una función que liste la nómina de productos artesanales con sello de excelencia, por año,
-disciplina y región. (referencia tabla 9.5). 
+disciplina y región. (referencia tabla 9.5).
 
 Debe considerar que un producto artesanal ha obtenido el sello de
 excelencia si tiene un valor en año_sello. La disciplina y región corresponden a datos del artesano que lo
